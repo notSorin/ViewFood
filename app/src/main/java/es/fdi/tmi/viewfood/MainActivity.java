c496ef498bbf6d04b7c10fecf579d0b88f691348
Sorin <sorindra@ucm.es>
2021-03-23 17:52:20 +0100

    Created base Android application.

---
+++ app/src/main/java/es/fdi/tmi/viewfood/MainActivity.java
@@ -0,0 +1,14 @@
+package es.fdi.tmi.viewfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
