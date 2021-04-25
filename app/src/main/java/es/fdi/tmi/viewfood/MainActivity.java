adad60c2034369271b35c6cfd12521c78c4139d4
Sorin <sorindra@ucm.es>
2021-04-25 16:58:00 +0200

    Added SharedPreferences to allow the user to save app preferences. Programmed the language selection dialog to always show the user's selected language, and to save it on disk when a new language is selected.

--- app/src/main/java/es/fdi/tmi/viewfood/MainActivity.java
+++ app/src/main/java/es/fdi/tmi/viewfood/MainActivity.java
@@ -1,10 +1,13 @@
 package es.fdi.tmi.viewfood;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 import androidx.exifinterface.media.ExifInterface;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -12,6 +15,7 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import java.io.File;
@@ -29,6 +33,7 @@ public class MainActivity extends AppCompatActivity
     private Uri _photoURI;
     private Button _translateButton, _photoButton;
     private TextView _translatedText;
+    private SharedPreferences _preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -36,6 +41,8 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initializeViews();
+
+        _preferences = getPreferences(Context.MODE_PRIVATE);
 
         //dispatchTakePictureIntent();
     }
@@ -83,12 +90,21 @@ public class MainActivity extends AppCompatActivity
 
         _languageSelector.setOnClickListener(v ->
         {
-            //TODO save the selected language on disk, and set the selected language each time the
-            //dialog is opened.
+            int selectedIndex = _preferences.getInt(getString(R.string.language_index), 0);
+
             new MaterialAlertDialogBuilder(MainActivity.this)
                     .setTitle("Translate into...")
-                    .setSingleChoiceItems(LANGUAGE_CHOICES, 0, null)
-                    .setPositiveButton("OK", null)
+                    .setSingleChoiceItems(LANGUAGE_CHOICES, selectedIndex, null)
+                    .setPositiveButton("OK", (dialog, which) ->
+                    {
+                        //Grab the selected index on the list and save it to disk.
+                        ListView lv = ((AlertDialog)dialog).getListView();
+                        int index = lv.getCheckedItemCount() == 1 ? lv.getCheckedItemPosition() : 0;
+                        SharedPreferences.Editor editor = _preferences.edit();
+
+                        editor.putInt(getString(R.string.language_index), index);
+                        editor.apply();
+                    })
                     .setNegativeButton("Cancel", null)
                     .show();
         });
