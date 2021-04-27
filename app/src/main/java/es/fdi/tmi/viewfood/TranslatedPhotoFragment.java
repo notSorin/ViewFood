8fdd3fadc8972c0531673323c10bab645bca8179
Sorin <sorindra@ucm.es>
2021-04-28 00:15:43 +0200

    Implemented the functionality of the TranslatedPhotoFragment.

--- app/src/main/java/es/fdi/tmi/viewfood/TranslatedPhotoFragment.java
+++ app/src/main/java/es/fdi/tmi/viewfood/TranslatedPhotoFragment.java
@@ -1,22 +1,37 @@
 package es.fdi.tmi.viewfood;
 
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.github.chrisbanes.photoview.PhotoView;
+import com.squareup.picasso.Picasso;
 
 public class TranslatedPhotoFragment extends Fragment
 {
+    private TextView _textPlaceholder;
+    private PhotoView _translatedPhoto;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View v = inflater.inflate(R.layout.fragment_translated_photo, container, false);
 
+        _textPlaceholder = v.findViewById(R.id.TranslatedPhotoPlaceholder);
+        _translatedPhoto = v.findViewById(R.id.TranslatedPhoto);
+
         return v;
+    }
+
+    public void setPhoto(String url)
+    {
+        _textPlaceholder.setVisibility(View.GONE);
+        _translatedPhoto.setVisibility(View.VISIBLE);
+        Picasso.get().load(url).into(_translatedPhoto);
     }
 }
