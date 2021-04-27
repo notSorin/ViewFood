0c309ee2574ca249bdceba6041c887a1c2869ded
Sorin <sorindra@ucm.es>
2021-04-28 00:06:08 +0200

    Added a text placeholder for the user taken photo. Changed from ImageView to PhotoView so the photo taken by the user can be interacted with.

--- app/src/main/java/es/fdi/tmi/viewfood/TakenPhotoFragment.java
+++ app/src/main/java/es/fdi/tmi/viewfood/TakenPhotoFragment.java
@@ -5,29 +5,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.github.chrisbanes.photoview.PhotoView;
 
 public class TakenPhotoFragment extends Fragment
 {
     private View _view;
-    private ImageView _takenImage;
+    private PhotoView _takenImage;
+    private TextView _textPlaceholder;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         _view = inflater.inflate(R.layout.fragment_taken_photo, container, false);
-        _takenImage = _view.findViewById(R.id.TakenImage);
+        _textPlaceholder = _view.findViewById(R.id.UserPhotoPlaceholder);
+        _takenImage = _view.findViewById(R.id.TakenPhoto);
 
         return _view;
     }
 
     public void setPhoto(Uri photoURI)
     {
+        _textPlaceholder.setVisibility(View.GONE);
+        _takenImage.setVisibility(View.VISIBLE);
         _takenImage.setImageURI(photoURI);
-        _takenImage.setScaleType(ImageView.ScaleType.FIT_CENTER);
     }
 }
